f855d56f82bea961d4ac18842f6ff4e40e127281
Nataly-Costaa <natalynaty653@gmail.com>
2024-11-05 17:21:06 -0300

    :tada: Commit inicial

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const main = document.querySelector('main');
+const newTask = document.getElementById('insertTask');
+const buttonAdd = document.getElementById('add');
+
+let createdTasks = [];
+
+buttonAdd.addEventListener('click', () => {
+    if (!newTask.value) {
+        alert('Insira uma task');
+        return;
+    } else if (createdTasks.includes(newTask.value)) {
+        alert('Essa task já exite');
+        newTask.value = '';
+        return;
+    }
+
+    creatNewList();
+    newTask.value = '';
+});
+
+function creatNewList(index) {
+    const newList = document.createElement('section');
+    newList.classList.add('containers');
+
+    newList.innerHTML = `
+        <section id="containers">
+            <input type="text" class="listTask" readonly>
+            <button class="delet" data-index="${index}">DELET</button>
+        </section>
+    `;
+
+    main.appendChild(newList);
+
+    const listTask = newList.querySelector('.listTask').value = newTask.value;
+    const buttonDelete = newList.querySelector('.delet');
+
+    
+    buttonDelete.addEventListener('click', () => {
+        // Encontra o índice da tarefa no array
+        const taskIndex = createdTasks.indexOf(listTask.value);
+        //Remove a tarefa do array
+        createdTasks.splice(taskIndex, 1);
+        
+        newList.remove();
+    });
+    
+    createdTasks.push(listTask);
+
+    return newList;
+}
